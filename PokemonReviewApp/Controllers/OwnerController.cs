36b4b19b24c9b5ade62f60686b90500b28b7361b
jess-nl <jnoriegalessard@gmail.com>
2024-05-02 13:05:02 -0400

    feat: add owners repository pattern

---
+++ PokemonReviewApp/Controllers/OwnerController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Dto;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OwnerController : Controller
+    {
+        private readonly IOwnerRepository _ownerRepository;
+
+        public OwnerController(IOwnerRepository ownerRepository)
+        {
+            _ownerRepository = ownerRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Owner>))]
+        public IActionResult GetOwners()
+        {
+            var owners = _ownerRepository.GetOwners();
+
+            var ownersDto = owners.Select(o => new OwnerDto
+            {
+                Id = o.Id,
+                FirstName = o.FirstName,
+                LastName = o.LastName,
+                Gym = o.Gym
+            });
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(ownersDto);
+        }
+
+        [HttpGet("{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+
+            var owner = _ownerRepository.GetOwner(ownerId);
+
+            var ownerDto = new OwnerDto
+            {
+                Id = owner.Id,
+                FirstName = owner.FirstName,
+                LastName = owner.LastName,
+                Gym = owner.Gym
+            };
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(ownerDto);
+        }
+
+        [HttpGet("{ownerId}/pokemon")]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+            {
+                return NotFound();
+            }
+
+            var pokemons = _ownerRepository.GetPokemonByOwner(ownerId);
+
+            var pokemonsDto = pokemons.Select(p => new PokemonDto
+            {
+                Id = p.Id,
+                Name = p.Name,
+                BirthDate = p.BirthDate
+            });
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemonsDto);
+        }
+    }
+}
--- PokemonReviewApp/Program.cs
+++ PokemonReviewApp/Program.cs
@@ -13,6 +13,7 @@ builder.Services.AddTransient<Seed>();
 builder.Services.AddScoped<IPokemonRepository, PokemonRepository>();
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<ICountryRepository, CountryRepository>();
+builder.Services.AddScoped<IOwnerRepository, OwnerRepository>();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
