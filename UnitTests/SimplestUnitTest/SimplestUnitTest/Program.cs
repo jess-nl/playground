09dd222248996a2cf94884e8b9a04f09d08bf8e2
jess-nl <jnoriegalessard@gmail.com>
2024-05-17 22:35:02 -0400

    feat: create simple unit tests

---
+++ UnitTests/SimplestUnitTest/SimplestUnitTest/Program.cs
@@ -0,0 +1,6 @@
+﻿using SimplestUnitTest.Tests;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+SimplestUnitFunctionTests.SimplestUnitFunction_ReturnsPikachuIfZero_ReturnsString();
---
+++ UnitTests/SimplestUnitTest/SimplestUnitTest/SimplestUnitFunction.cs
@@ -0,0 +1,17 @@
+﻿namespace SimplestUnitTest
+{
+    public class SimplestUnitFunction
+    {
+        public string ReturnsPikachuIfZero(int num)
+        {
+            if (num == 0)
+            {
+                return "PIKACHU!";
+            }
+            else
+            {
+                return "Squirtle";
+            }
+        }
+    }
+}
